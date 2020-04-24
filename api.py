7fbab79e787ad4fe64da3d1a8f95a40e569381cc
Paul C. Zimmerman <paul@lugal.com>
2020-04-23 22:55:00 -0400

    Make return responses more consistent and concise.

--- api.py
+++ api.py
@@ -10,83 +10,75 @@ app = FastAPI()
 def azimuth_set(response: Response, degrees: int=0, minutes: int=0, seconds: int=0):
     """Sets the azimuth on the total station."""
     result = engine.total_station.set_azimuth(degrees, minutes, seconds)
-    if result['success']:
-        return result['azimuth']
-    else:
+    if 'errors' in result:
         response.status_code = 422
-        return result['errors']
+    return result
 
 
 @app.get('/instrument_height/')
 def instrument_height_get():
     """"Gets the instrument height above the occupied point."""
     result = engine.station.get_instrument_height()
-    return result['instrument_height']
+    return result
 
 
 @app.post('/instrument_height/')
 def instrument_height_set(response: Response, height: float):
     """"Gets the instrument height above the occupied point."""
     result = engine.station.set_instrument_height(height)
-    if result['success']:
-        return result['instrument_height']
-    else:
+    if 'errors' in result:
         response.status_code = 422
-        return result['errors']
+    return result
 
 
 @app.get('/cancel/')
 def measurement_cancel():
     """Stops a measurement in progress."""
     result = engine.total_station.cancel_measurement()
-    return result['result']
+    return result
 
 
 @app.get('/measurement/')
 def measurement_take(response: Response):
     """Tells the total station to start measuring a point."""
     result = engine.total_station.take_measurement()
-    if result['success']:
-        return engine.data.apply_offsets_to_measurement(result)['measurement']
-    else:
+    if 'errors' in result:
         response.status_code = 422
-        return result['errors']
+    else:
+        result = engine.data.apply_offsets_to_measurement(result)
+    return result
 
 
 @app.post('/mode_hr/')
 def mode_hr_set(response: Response):
     """Sets the total station to horizontal right mode."""
     result = engine.total_station.set_mode_hr()
-    if result['success']:
-        return result['result']
-    else:
+    if 'errors' in result:
         response.status_code = 422
-        return result['errors']
+    return result
 
 
 @app.get('/occupied_point/')
 def occupied_point_get():
     """Gets the coordinates of the occupied point."""
     result = engine.station.get_occupied_point()
-    return result['coordinates']
+    return result
 
 
 @app.post('/occupied_point/')
 def occupied_point_set(response: Response, northing: float, easting: float, elevation: float):
     """Sets the coordinates of the occupied point."""
     result = engine.station.set_occupied_point(northing, easting, elevation)
-    if result['success']:
-        return result['coordinates']
-    else:
+    if 'errors' in result:
         response.status_code = 422
-        return result['errors']
+    return result
 
 
 @app.get('/prism_offset/')
 def prism_offset_get():
     """Gets the prism offsets."""
     result = engine.prism.get_prism_offset()
-    return result['prism_offset']
+    return result
 
 
 @app.post('/prism_offset/')
@@ -94,7 +86,5 @@ def prism_offset_set(response: Response, offsets: dict):
     """Sets the prism offsets."""
     result = engine.prism.set_prism_offset(**offsets)
     if result['success']:
-        return result['prism_offset']
-    else:
         response.status_code = 422
-        return result['errors']
+    return result
