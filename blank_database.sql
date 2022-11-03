11412e455bf2e8f4f8fe444799a0542f0f3fbab5
Paul Zimmerman <paul@lugal.com>
2022-11-03 06:37:50 +0300

    Update README for new interface changes.

--- blank_database.sql
+++ blank_database.sql
@@ -116,12 +116,12 @@ CREATE TABLE `subclasses` (
 INSERT INTO subclasses VALUES(1,1,'Survey Station','Benchmarks for survey station setup or backsights.');
 INSERT INTO subclasses VALUES(2,1,'Grid','Site or survey grid.');
 INSERT INTO subclasses VALUES(3,1,'Trench','Excavation units.');
-INSERT INTO subclasses VALUES(4,1,'Elevation Control Point','Control point for taking local elevations.');
+INSERT INTO subclasses VALUES(4,1,'Elevation Control Point','Control point for taking local elevations, as with a string and bubble level.');
 INSERT INTO subclasses VALUES(5,1,'GCP','Photogrammetry ground control points.');
 INSERT INTO subclasses VALUES(6,2,'Wall','Vertical, human-made, constructions that enclose, divide, or delimit space.');
 INSERT INTO subclasses VALUES(7,2,'Floor','Level surfaces upon which human activities took place.');
 INSERT INTO subclasses VALUES(8,4,'Topography','Ground surface.');
-INSERT INTO subclasses VALUES(9,4,'Pit','');
+INSERT INTO subclasses VALUES(9,4,'Pit','Hole or depression that cuts through lower stratigraphic layers.');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('classes',4);
 INSERT INTO sqlite_sequence VALUES('geometries',4);
